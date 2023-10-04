23517a03dd577431df610f8740a1fe43054e3f9c
brsaca <brsaca@gmail.com>
2023-10-04 14:38:54 -0600

    feat: initial structure, add colors

---
+++ HabitsUI/Extension/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  HabitsUI
+//
+//  Created by Brenda Saavedra Cantu on 04/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let cBlack = Color("black")
+    static let cBlue = Color("blue")
+    static let cYellow = Color("yellow")
+}
