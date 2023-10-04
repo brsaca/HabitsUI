4d098a6e4c740b557f851758b407634a07b82d43
brsaca <brsaca@gmail.com>
2023-10-04 13:58:05 -0600

    Initial Commit

---
+++ HabitsUI/HabitsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsUIApp.swift
+//  HabitsUI
+//
+//  Created by Brenda Saavedra Cantu on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
